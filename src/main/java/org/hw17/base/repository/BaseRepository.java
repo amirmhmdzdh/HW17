74644b7dbb2ee3bb9264388f036eebe525e99aeb
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-27 20:20:23 +0430

    Create Class and method

---
+++ src/main/java/org/hw17/base/repository/BaseRepository.java
@@ -0,0 +1,20 @@
+package org.hw17.base.repository;
+
+import org.hw17.base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void delete(T entity);
+
+    List<T> findAll();
+
+    List<T> showAll();
+}
