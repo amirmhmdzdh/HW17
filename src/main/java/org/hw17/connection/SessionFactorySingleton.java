892043f10b9421d3752c046afc8f8b483acf1e3c
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-27 23:30:20 +0430

    Create Class and feild

---
+++ src/main/java/org/hw17/connection/SessionFactorySingleton.java
@@ -0,0 +1,31 @@
+package org.hw17.connection;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hw17.model.BankAccount;
+import org.hw17.model.Debt;
+import org.hw17.model.Student;
+
+public class SessionFactorySingleton {
+    private SessionFactorySingleton() {
+    }
+
+    private static class SessionFactoryHelper {
+        static org.hibernate.service.ServiceRegistry registry =
+                new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build();
+        private static final SessionFactory INSTANCE =
+                new MetadataSources(registry)
+                        .addAnnotatedClass(Student.class)
+                        .addAnnotatedClass(Long.class)
+                        .addAnnotatedClass(Debt.class)
+                        .addAnnotatedClass(BankAccount.class)
+                        .buildMetadata()
+                        .buildSessionFactory();
+    }
+    public static SessionFactory getInstance() {
+        return SessionFactoryHelper.INSTANCE;
+    }
+}
