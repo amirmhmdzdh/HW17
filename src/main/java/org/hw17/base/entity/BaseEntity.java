7c59280dc1df9edceb8a0331ccac5b7bdcea2d06
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-27 20:11:07 +0430

    Create Class and field Id

---
+++ src/main/java/org/hw17/base/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package org.hw17.base.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+public class BaseEntity<ID extends Serializable> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+}
